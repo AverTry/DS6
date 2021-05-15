849ad4686481200af00fc0e59f2c33d302dc52cb
AverTry <avertry@gmail.com>
2021-05-15 19:11:43 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+const tailwindcss = require("tailwindcss");
+
+module.exports = {
+  // mode: 'jit',
+  purge: {
+    enabled: false, // or true to purge unused css
+    preserveHtmlElements: false,
+    content: ['dist/*.html'], // files checked for purging
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter"],
+      },
+      transformOrigin: {
+        "0": "0%",
+      },
+      zIndex: {
+        "-1": "-1",
+      },
+    },
+  },
+  variants: {
+    borderColor: ['responsive', 'hover', 'focus', 'focus-within'],
+    extend: {},
+  },
+  plugins: [
+    require("tailwindcss"),
+    require('@tailwindcss/forms'),
+  ],
+  corePlugins: {
+    appearance: false,
+  }
+}
